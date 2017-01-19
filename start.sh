18145ee9e01ea6c56d2d0c8388b54ddacd67a0f8
Gergely Imreh <imrehg@gmail.com>
2017-01-19 19:42:02 +0000

    better locking example

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-while : ; do
-  echo "Idling & locked: ${COUNTER}"
-  sleep 600
-done
+# Makes sure we exit if flock fails.
+set -e
+
+# The application you'd like to run
+main () {
+  while : ; do
+    echo "Idling & locked: ${COUNTER}"
+    sleep 600
+  done
+}
+
+# Update locking and running the code
+(
+
+  flock -x -n 200
+  main
+
+) 200>/data/resin-updates.lock
